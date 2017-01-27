0de4b7f0c260f99a8c55602e94e518732870cfd5
lnardi <lnardi@gmail.com>
2017-01-27 17:34:56 -0200

    Testes genericos

--- src/br/com/mavalu/test/GenericTest.java
+++ src/br/com/mavalu/test/GenericTest.java
@@ -18,17 +18,17 @@ public class GenericTest {
    public static void main(String args[]) {
       /* Set the Nimbus look and feel */
 
-      Pattern PATTERN = Pattern.compile("[^A-Za-z0-9çÇ   ã\t\nÃõÕ_\\-\\.]");
+      //Pattern PATTERN = Pattern.compile("[^A-Za-z0-9çÇ   ã\t\nÃõÕ_\\-\\.]");
 
       //String a = "::CONCEI?AO DA PAIXAO MACIEL DE SA DOS|| SANTOç:ÇS* - 3484146.pdf -|";
-      String a = "TAÃ??S CA   R\t\nLA MA\\GA//LH>Ã<ass?Æ?E\"S";
+      String a = "TAÃ??S CA   R\t\nLA MA\\GA//LH>Ã<as;;;s?Æ?E\"S";
       System.out.println(a);
       // String a = "TAÃiS CARLA MAnGALHÃES";
 
       //System.out.println("String: " + a.replaceAll("[^A-Za-z0-9Ç\\ÃÕÔ//ÁÉÊÛ_\\s_\\-\\.]", "#"));
       //System.out.println("String: " + a.replaceAll("[\\?]", "#"));
-      System.out.println("String: " + a.replaceAll("[\\t\\n\\\\\\/\\:\\*\\?\\\"\\<\\>\\|]", "@"));
-      
+      //System.out.println("String: " + a.replaceAll("[\\;\\t\\n\\\\\\/\\:\\*\\?\\\"\\<\\>\\|]", "@"));
+      System.out.println("String: " + a.replaceAll("\\;|\\r|\\n", "#"));
 
       /**
        * try {
